e51327bf969e7b60485cd3187bd84ceaca764842
RODRIGO COSTA SANTOS <rodrigo.costa@gps-pamcary.com.br>
2021-10-22 09:49:07 -0300

    init go client

---
+++ goClient/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	oidc "github.com/coreos/go-oidc"
+	"golang.org/x/oauth2"
+)
+
+var (
+	clientID     = "myclient"
+	clientSecret = "e70c30db-1323-4deb-8eea-d5a0eb953de2"
+)
+
+func main() {
+	ctx := context.Background()
+	provider, err := oidc.NewProvider(ctx, "http://localhost:8080/auth/realms/myrealm")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Endpoint:     provider.Endpoint(),
+		RedirectURL:  "http://localhost:8081/auth/callback",
+		Scopes:       []string{oidc.ScopeOpenID, "profile", "email", "roles"},
+	}
+
+	state := "AnySecurityNumber123"
+
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		http.Redirect(writer, request, config.AuthCodeURL(state), http.StatusFound)
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
